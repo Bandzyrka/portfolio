222faff2e7195ab0a7deda50bcec4b114f1669ed
Wiktor Bandzyra <werstaptor@gmail.com>
2022-04-15 22:39:27 +0200

    Added navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-
+import NavBar from './components/NavBar/NavBar.component'
 import './App.scss';
 
 const App = () => (
   <div className="app">
-    
+    <NavBar />
   </div>
 );
 
