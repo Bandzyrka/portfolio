8ab87c19b3d1da974235527528f1582c76fde337
Wiktor Bandzyra <werstaptor@gmail.com>
2022-04-15 23:09:02 +0200

    Added header container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import NavBar from './components/NavBar/NavBar.component'
+import Header from './containers/Header/Header.component'
 import './App.scss';
 
 const App = () => (
   <div className="app">
     <NavBar />
+    <Header />
   </div>
 );
 
